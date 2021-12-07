ff64e648389de277bdac31baa74d7697742d16e3
James Johnson <huminater@gmail.com>
2021-12-07 04:41:46 +0000

    Add initial video components

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ import Container from 'react-bootstrap/Container';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+function Video(props) {
   return (
     <>
+      <h1>{props.videoTitle}</h1>
+      <div className="video-wrapper">
+        <div className="video-container">
+          <video width="640" height="380" className='video-tag' controls>
+            <source src={props.fileName} type='video/mp4' />
+          </video>
+        </div>
+      </div>
+      <a className="link" href={props.link} target="_blank" rel="noopener noreferrer">{props.linkText}</a>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand>James Johnson</Navbar.Brand>
@@ -17,12 +33,23 @@ function App() {
         </Container>
       </Navbar>
 
-      <div class="App">
-        <header class="App-header">
-
-        </header>
-      </div>
-    </>
+      <header className="App-header">
+        <Video 
+          videoTitle="Bob Ross the Boss"
+          fileName="./bobrosstheboss.mp4" 
+          link="https://wet-dog.github.io/BobRossTheBossWebGL/"
+          linkText="Try it out here"
+        >
+        </Video>
+        <Video 
+          videoTitle="Glow Runners"
+          fileName="./glowrunners.mp4" 
+          link="https://briggsywxf.github.io/bathhack21/"
+          linkText="Try it out here"
+        >
+        </Video>
+      </header>
+    </div>
   );
 }
 
